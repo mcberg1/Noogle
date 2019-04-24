54f4bba0d522ff7293889c6bf4193de51d128463
jkmosa01 <35510468+jkmosa01@users.noreply.github.com>
2019-04-24 16:17:47 -0600

    ALL THE CHANGES

---
+++ index.js
@@ -0,0 +1,33 @@
+function onload(){
+    document.body.style.backgroundColor = randomColor();
+    document.body.style.color = randomColor();
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random()*256).toString(16);
+    var g = Math.floor(Math.random()*256).toString(16);
+    var b = Math.floor(Math.random()*256).toString(16);
+    if(r.length==1){r="0"+r}
+    if(g.length==1){g="0"+g}
+    if(b.length==1){b="0"+b}
+    return "#"+r+g+b
+}
+
+function search(value){
+    var request = new XMLHttpRequest();
+    if(value==""){
+        document.getElementById("title").innerHTML="";
+        document.getElementById("snippet").innerHTML="";
+    }
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=&list=search&titles=50&utf8=1&srsearch='+encodeURI(value)+'&srlimit=1&srwhat=text&srinfo=totalhits&srprop=wordcount%7Csnippet&srenablerewrites=1&srsort=incoming_links_asc', true);
+    request.onload = function () {
+        let obj = JSON.parse(this.responseText);
+        console.log(obj.query.search);
+        document.getElementById("snippet").innerHTML = obj.query.search[0].snippet;
+        document.getElementById("title").innerHTML = obj.query.search[0].title;
+        console.log(obj.query);
+    }
+    // Send request
+    request.send()
+}
